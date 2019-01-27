37a367625a282bccdff5b06777ec5d579a738f2b
fereste <fernando.piubel@gmail.com>
2019-01-27 18:20:19 -0300

    Primer build

---
+++ precache-manifest.d4ad49c1f38796dd100030abaecdb039.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a913b3cb4dcda955d9bd",
+    "url": "/static/js/main.a913b3cb.chunk.js"
+  },
+  {
+    "revision": "93c37be4c979d12abf72",
+    "url": "/static/js/1.93c37be4.chunk.js"
+  },
+  {
+    "revision": "a913b3cb4dcda955d9bd",
+    "url": "/static/css/main.787d1369.chunk.css"
+  },
+  {
+    "revision": "ff0d198b61d9e972a2ad7f161775336d",
+    "url": "/index.html"
+  }
+];
