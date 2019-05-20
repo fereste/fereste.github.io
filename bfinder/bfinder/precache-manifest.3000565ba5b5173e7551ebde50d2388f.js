f27eb9b94c3bc7fd1c5d7a429530d9d7bb5a4bce
fereste <fernando.piubel@gmail.com>
2019-05-20 00:25:42 -0300

    bfinder

---
+++ bfinder/bfinder/precache-manifest.3000565ba5b5173e7551ebde50d2388f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38909082666248addf7b9289234bbfa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3f3c9e9f4b3d65f3c24b",
+    "url": "./static/css/main.1af0f89b.chunk.css"
+  },
+  {
+    "revision": "00a1c2f79b802c763fa6",
+    "url": "./static/js/2.2919a61f.chunk.js"
+  },
+  {
+    "revision": "3f3c9e9f4b3d65f3c24b",
+    "url": "./static/js/main.32dc569f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
