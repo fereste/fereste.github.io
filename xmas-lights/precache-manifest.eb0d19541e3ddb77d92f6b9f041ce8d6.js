00231004687caf4b0326b7debfc75d4717adb193
fereste <fernando.piubel@gmail.com>
2019-12-08 16:21:34 -0300

    Update

---
+++ xmas-lights/precache-manifest.eb0d19541e3ddb77d92f6b9f041ce8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039e2c8945bd79ff5a6f882a7f7aaa94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c37f605cfaffc8414be2",
+    "url": "./static/css/main.f1b42223.chunk.css"
+  },
+  {
+    "revision": "cf5ab3bb3f674a4526da",
+    "url": "./static/js/2.49a656a0.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.49a656a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c37f605cfaffc8414be2",
+    "url": "./static/js/main.2d8ff634.chunk.js"
+  },
+  {
+    "revision": "62b768cb7e0139cbccfe",
+    "url": "./static/js/runtime-main.a7d4fd0a.js"
+  }
+]);
